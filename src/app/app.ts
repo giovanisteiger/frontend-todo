10efbb4a9c8d12236a21defbcd05c86343e0fc50
Giovani <giovanisteiger@gmail.com>
2026-04-27 20:28:40 -0300

    add frontend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import { Tarefa } from "./tarefa";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,47 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('TODOapp');
+
+  // ✅ AGORA É SÓ SIGNAL (sem duplicação)
+  arrayDeTarefas = signal<Tarefa[]>([]);
+
+  apiURL: string;
+
+  constructor(private http: HttpClient) {
+    this.apiURL = 'http://localhost:3000';
+    this.READ_tarefas();
+  }
+
+  // ✅ CREATE usando signal
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    const novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+
+     this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
+
+  // ✅ READ usando set
+  READ_tarefas() {
+     this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
+      resultado => this.arrayDeTarefas.set(resultado));
+  }
+
+  // ✅ DELETE usando update + filter
+  removerTarefa(tarefaAserRemovida: Tarefa) {
+     var indice = this.arrayDeTarefas().indexOf(tarefaAserRemovida); 
+    var id = this.arrayDeTarefas()[indice]._id;                     
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
+
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    var indice = this.arrayDeTarefas().indexOf(tarefaAserModificada); 
+    var id = this.arrayDeTarefas()[indice]._id;                      
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
+      tarefaAserModificada).subscribe(
+        resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
+
 }
---
+++ src/app/item/item.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Tarefa } from "../tarefa";
+
+@Component({
+  selector: 'app-item',
+  standalone: false,
+  templateUrl: './item.html',
+  styleUrl: './item.css',
+})
+export class Item {
+  emEdicao = false;
+  @Input() tarefa: Tarefa = new Tarefa("", false);
+
+  @Output() remover = new EventEmitter<Tarefa>();
+
+  @Output() modificaTarefa = new EventEmitter();
+
+  removerTarefa() {
+    this.remover.emit(this.tarefa);
+  }
+}
+
